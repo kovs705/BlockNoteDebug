06e9abef872f7b0ce665872153ac52ef488102bd
kovs705 <smartkovsik@gmail.com>
2021-12-04 12:16:37 +0300

    Okay, so I realized how this works, I have something in my C1NavigationView in my BlockNote app that causes crashing of the view, I think I need to check what exactly goes on. DONE

---
+++ BlockNoteDebug/Views/ContentView.swift
@@ -0,0 +1,87 @@
+//
+//  ContentView.swift
+//  BlockNoteDebug
+//
+//  Created by Kovs on 03.12.2021.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    @FetchRequest(entity: Note.entity(), sortDescriptors: [NSSortDescriptor(key: "noteID", ascending: true)], predicate: nil) var notes: FetchedResults<Note>
+    @FetchRequest(entity: GroupType.entity(), sortDescriptors: [NSSortDescriptor(key: "number", ascending: true)], predicate: nil) var groups: FetchedResults<GroupType>
+    
+    let columns = [GridItem(.flexible()), GridItem(.flexible())]
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                VStack {
+                    // MARK: - Groups
+                    LazyVGrid(columns: columns, spacing: 10) {
+                        ForEach(groups, id: \.self) { group in
+                            NavigationLink(destination: GroupDetailView(groupType: group)) {
+                                Text(group.wrappedGroupName)
+                                    .font(.system(size: 17))
+                            }
+                        }
+                    }
+                    .padding()
+                    
+                }
+                // VStack
+            }
+            .frame(width: UIScreen.main.bounds.width - 20, height: 700)
+            
+            // scroll
+            .navigationBarItems(trailing: Button(action: {
+                addItem()
+            }) {
+                Image(systemName: "plus")
+            })
+            // ZStack
+        }
+        .ignoresSafeArea(.all)
+        // NaView
+        
+    }
+
+    private func addItem() {
+        withAnimation {
+            let newGroup = GroupType(context: viewContext)
+            
+            newGroup.groupName = "Checking.."
+            newGroup.number = (groups.last?.number ?? 0) + 1
+            newGroup.groupColor = "RedStrawBerry"
+            
+            do {
+                try self.viewContext.save()
+                print("Group is added!")
+            } catch {
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+
+    private func deleteGroups(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { groups[$0] }.forEach(viewContext.delete)
+
+            do {
+                try viewContext.save()
+            } catch {
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}
